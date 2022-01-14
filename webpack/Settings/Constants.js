a97bfb366c803a05724bb9d8ba5093701669f685
s5delement <s5@d-element.ru>
2022-01-14 11:47:51 +0500

    Собранный проект

--- webpack/Settings/Constants.js
+++ webpack/Settings/Constants.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = {
     dirs: {
         src: path.join(__dirname, "../", "../", "src"),
-        dist: path.join(__dirname, "../", "../", "dist")
+        dist: path.join(__dirname, "../", "../", "docs")
     }
 }
